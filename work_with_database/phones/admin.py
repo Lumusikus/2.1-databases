ee584216f280f587e6693289cd7627f1ff2c198c
Lumusikus <151466557+Lumusikus@users.noreply.github.com>
2025-09-18 11:46:14 +0300

    перезагружаем репозиторий

--- work_with_database/phones/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Phone
-
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'release_date', 'lte_exists')
-    prepopulated_fields = {'slug': ('name',)}
